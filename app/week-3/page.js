6224035ea0e4aeb831d2c638f5ba5d5219813253
EBUBE VINCENT <124905297+monterovincent@users.noreply.github.com>
2026-09-19 18:17:46 -0600

    Center shopping list on week-3 page

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,8 +3,10 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <main className="bg-slate-950 min-h-screen p-4">
+            <div className="max-w-md mx-auto">
             <h1 className="text-3xl font-bold text-sky-100 mb-4">Shopping List</h1>
             <ItemList/>
+            </div>
         </main>
     )
 
