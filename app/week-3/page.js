00261607ffd685c4ecc8978c58dc51da7b79a32e
Azuka Olisemelie David <johntwice8@gmail.com>
2026-09-22 12:57:09 -0600

    refactor Item List

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,6 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <main>
-            <h1 className="title">Week 3 Assignment</h1>
             <ItemList />
         </main>
     );
